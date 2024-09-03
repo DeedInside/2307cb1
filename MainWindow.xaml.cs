a65c49e3378cf099ec4a2fb23aa2dfbfb50cfc72
Пауль Сергей Александрович <paul_s_a@mail.ru>
2024-09-03 15:03:29 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace _2307cb1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
