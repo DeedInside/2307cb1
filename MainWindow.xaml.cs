041722a3a213e482b24c5a8e1a3c2d7565fe66b1
Пауль Сергей Александрович <paul_s_a@mail.ru>
2024-09-17 16:03:30 +0700

    17.09

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System.Windows;
+﻿using _2307cb1.Model;
+using System.ComponentModel.DataAnnotations;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace _2307cb1
 {
     public partial class MainWindow : Window
     {
+        List<User> users;
         private int b;
         public int B
         {
@@ -14,7 +17,7 @@ namespace _2307cb1
             }
             set
             {
-                if( value >= 0)
+                if (value >= 0)
                 {
                     b = value;
                 }
@@ -22,19 +25,92 @@ namespace _2307cb1
         }
 
         int a = 9;
+        
+        string str { get; set; } = "";
+
         public MainWindow()
         {
             InitializeComponent();
-            int a = 9;
+            users = new List<User>()
+            {
+                new User("Name 1", "Email", "Pass"),
+                new User("Name 2", "Email", "Pass"),
+                new User("Name 3", "Email", "Pass"),
+                new User("Name 4", "Email", "Pass"),
+            };
 
+            ListBoxUser.ItemsSource = users;
+           // MessageBox.Show(users[0].Id.ToString());
+
+            check_1.IsChecked = true;
+
+            ComboBoxUserName.ItemsSource = toStrUserName(users);
         }
 
         private void bt_out_text(object sender, RoutedEventArgs e)
         {
-            
+
             Button bt = sender as Button;
             bt.Content = "туц туц туц";
             lb.Content = tb_1.Text;
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox check = sender as CheckBox;
+            if (check.IsChecked == true)
+            {
+                stackPanObr.Visibility = Visibility.Visible;
+                MessageBox.Show(stackPanObr.Visibility.ToString());
+            }
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            stackPanObr.Visibility = Visibility.Collapsed;
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rad = sender as RadioButton;
+
+            if (rad.IsChecked == true)
+            {
+                MessageBox.Show(rad.Content.ToString());
+            }
+        }
+
+        private void Check_listBox(object sender, SelectionChangedEventArgs e)
+        {
+            ListBox list = sender as ListBox;
+
+            if( list != null)
+            {
+                MessageBox.Show(list.SelectedItem.ToString());
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            users.Add(new User("new name", "new Email", "new pass"));
+            ListBoxUser.Items.Refresh();
+        }
+
+        public List<string> toStrUserName(List<User> users)
+        {
+            List<string> result = new List<string>();
+
+            foreach (var item in users)
+            {
+                result.Add(item.Name);
+            }
+            return result;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            str = TestTextBox.Text.ToString();
+            MessageBox.Show(str);
         }
     }
 }
---
+++ Model/User.cs
@@ -0,0 +1,34 @@
+﻿namespace _2307cb1.Model
+{
+    /// <summary>
+    /// public commit
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// commit
+        /// </summary>
+        /// <param name="name">commit params 1</param>
+        /// <param name="email">commit params 2</param>
+        /// <param name="password">commit params 3</param>
+        public User(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+        /// <summary>
+        ///  indentity user
+        /// </summary>
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        public override string ToString()
+        {
+            return $"{Name} - {Email} - {Email} - {Password}";
+        }
+    }
+}
